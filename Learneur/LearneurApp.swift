01dc4f3f8499e56468bc117511cc91d4b4a001b2
Piterong <piterwongso6@gmail.com>
2022-07-22 15:42:23 +0700

    Initial Commit

---
+++ Learneur/LearneurApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearneurApp.swift
+//  Learneur
+//
+//  Created by Piter Wongso on 22/07/22.
+//
+
+import SwiftUI
+
+@main
+struct LearneurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
